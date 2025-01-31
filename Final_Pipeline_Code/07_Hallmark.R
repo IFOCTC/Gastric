929346276133817571406f8494cc782157928542
CTC <davide.mascolo@ifo.it>
2025-01-31 15:42:51 +0100

    Add files via upload

--- Final_Pipeline_Code/07_Hallmark.R
+++ Final_Pipeline_Code/07_Hallmark.R
@@ -101,42 +101,15 @@ df_tpm_own_filtered_tumor <- df_tpm_own %>%
 rownames(df_tpm_tcga_cleaned_tumor) <- make.names(genes_names_tcga, unique = TRUE) 
 rownames(df_tpm_own_filtered_tumor) <- make.names(genes_names_tpm, unique = TRUE) 
 
-## Define the list of genes to use
-genes <- c("GNAI2", "RSU1", "NRP1", "GNS", "IFITM3",
-           "MMP14", "FYN", "TUBA1A", "TNFRSF1A", "CTSZ",
-           "CLIC4", "TNIP1", "C1S", "HIF1A", "PGK1",
-           "PIP4K2A", "LGALS1", "SPARC", "SERPINH1", "ECE1",
-           "FBN1", "TMSB10", "DCTN2", "PDLIM1", "GNAS",
-           "DBN1", "IGFBP4", "CTSD", "C3", "NONO",
-           "ITGB1", "LTBP3", "MYADM", "COL18A1", "CAV1",
-           "CD68", "CUL4B", "C1R", "SPON2", "ABCA1",
-           "TAF10", "EMP3", "CSGALNACT2", "CD44", "GRN",
-           "PLP2", "RBM3", "PHF11", "SYNPO", "GDI1",
-           "CTSH", "DSE", "GABARAPL1", "ZEB1", "VDAC2",
-           "SMAD3", "FAM3C", "VIM", "MORF4L2", "NRBP1",
-           "PRNP", "TUBB6", "RAP1B", "CD82", "ARFGAP1",
-           "NRP2", "VPS26A", "PML", "TAGLN", "SART1",
-           "CLDND1", "SERPINE1", "RAB8B", "TPP1", "UGP2",
-           "MPI", "CAPG", "SEMA3C", "ARHGEF10", "AAK1",
-           "MCU", "RUNX1", "IL1RAP", "DCTN1", "NDST1",
-           "UROD", "SLC25A28", "AP1S2", "TMSB4X", "SLC1A3",
-           "DLGAP4", "DENND3", "HCLS1", "LAMB3", "FIBP",
-           "NAMPT", "MARS", "CTSA", "EIF3E", "LGALS8",
-           "CYR61", "LGALS9")
-
-## Select only signature genes columns
-df_tpm_own_filtered_tumor_signature <- df_tpm_own_filtered_tumor %>% 
-  filter(rownames(df_tpm_own_filtered_tumor) %in% genes)
-
+## OWN
 ## Load Hallmark
 hallmark_pathways   <- getGmt(paste0(data_path_own, "/h.all.v2023.2.Hs.symbols.gmt"))
-expression_data     <- as.matrix(df_tpm_own_filtered_tumor_signature)
+expression_data     <- as.matrix(df_tpm_own_filtered_tumor)
 ssgsea_params       <- ssgseaParam(expression_data, hallmark_pathways)
 ssGSEA_scores       <- gsva(ssgsea_params)
 ssGSEA_scores       <- as.data.frame(t(ssGSEA_scores))
 ssGSEA_scores_scale <- scale(ssGSEA_scores)
 ssGSEA_scores_scale <- data.frame(ssGSEA_scores_scale)
-
 ## Wrangling
 ssGSEA_scores_scale$ID <- rownames(ssGSEA_scores_scale)
 ## Merge
@@ -144,54 +117,18 @@ ssGSEA_scores_scale_final <- merge(ssGSEA_scores_scale, df_signature_own, by = "
 rownames(ssGSEA_scores_scale_final) <- ssGSEA_scores_scale_final$ID
 ssGSEA_scores_scale_final$ID <- NULL
 ssGSEA_scores_scale_final$Genes <- NULL 
-
-## Reshape
-df_long_own <- tidyr::pivot_longer(ssGSEA_scores_scale_final, cols = starts_with("HALLMARK"), 
-                                   names_to = "Variable", values_to = "Value")
-## Plot
-hallmark_unique <- unique(df_long_own$Variable)
-for (hallmark in hallmark_unique){
-  df_subset <- subset(df_long_own, Variable == hallmark)
-  p <- ggplot(df_subset,
-              aes(x = factor(Signature, levels = c("Low", "High")),
-                  y = Value, fill = Signature)) +
-    geom_boxplot(outlier.size = 1, outlier.colour = "black",
-                 width = 0.7, colour = "black", alpha = 0.8) +
-    labs(title = paste0(hallmark, "- IRE"),
-         x = "Signature",
-         y = "Value Expression",
-         fill = "") +
-    scale_fill_manual(values = c("Low" = "#1f77b4", "High" = "#d62728")) +
-    theme_minimal(base_size = 14) +
-    theme(legend.position = "",
-          axis.text.x = element_text(angle = 45, hjust = 1, size = 12, face = "plain"),
-          axis.text.y = element_text(size = 12, face = "plain"),
-          plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
-          axis.title = element_text(size = 14, face = "bold"),
-          panel.grid.major = element_line(size = 0.2, color = "gray90"),
-          panel.grid.minor = element_blank(),
-          plot.background = element_rect(fill = "white", color = "white")) +
-    stat_compare_means(method = "kruskal.test", label = "p.signif", label.x = 1.5, size = 5)
-  ggsave(paste0("C:/Users/david/Documents/IFO/Final_Pipeline_Code/Output_Images/Hallmark/IRE/",
-                hallmark, "_boxplot.png"), plot = p, width = 8, height = 6)}
-
-
-
+ssGSEA_scores_scale_final$Class <- rep("IRE", nrow(ssGSEA_scores_scale_final))
 
 ## TCGA
-## Select only signature genes columns
-df_tpm_tcga_cleaned_tumor_signature <- df_tpm_tcga_cleaned_tumor %>% 
-  filter(rownames(df_tpm_tcga_cleaned_tumor) %in% genes)
-
 ## Load Hallmark
-hallmark_pathways   <- getGmt(paste0(data_path_own, "/h.all.v2023.2.Hs.symbols.gmt"))
-expression_data_tcga     <- as.matrix(df_tpm_tcga_cleaned_tumor_signature)
+hallmark_pathways        <- getGmt(paste0(data_path_own, "/h.all.v2023.2.Hs.symbols.gmt"))
+expression_data_tcga     <- as.matrix(df_tpm_tcga_cleaned_tumor)
 ssgsea_params_tcga       <- ssgseaParam(expression_data_tcga, hallmark_pathways)
 ssGSEA_scores_tcga       <- gsva(ssgsea_params_tcga)
 ssGSEA_scores_tcga       <- as.data.frame(t(ssGSEA_scores_tcga))
 ssGSEA_scores_scale_tcga <- scale(ssGSEA_scores_tcga)
 ssGSEA_scores_scale_tcga <- data.frame(ssGSEA_scores_scale_tcga)
-
+ssGSEA_scores_scale_tcga$Class <- rep("TCGA", nrow(ssGSEA_scores_scale_tcga))
 ## Wrangling
 ssGSEA_scores_scale_tcga$ID <- rownames(ssGSEA_scores_scale_tcga)
 ## Merge
@@ -199,20 +136,103 @@ ssGSEA_scores_scale_final_tcga <- merge(ssGSEA_scores_scale_tcga, df_signature_t
 rownames(ssGSEA_scores_scale_final_tcga) <- ssGSEA_scores_scale_final_tcga$ID
 ssGSEA_scores_scale_final_tcga$ID <- NULL
 ssGSEA_scores_scale_final_tcga$Genes <- NULL 
+ssGSEA_scores_scale_final_tcga$Class <- rep("TCGA", nrow(ssGSEA_scores_scale_final_tcga))
+
+## Merge
+df_final_hallmark <- rbind(ssGSEA_scores_scale_final, ssGSEA_scores_scale_final_tcga)
 
 ## Reshape
-df_long_tcga <- tidyr::pivot_longer(ssGSEA_scores_scale_final_tcga, cols = starts_with("HALLMARK"), 
-                                    names_to = "Variable", values_to = "Value")
+df_final_hallmark_long <- tidyr::pivot_longer(df_final_hallmark, cols = starts_with("HALLMARK"), 
+                                              names_to = "Variable", values_to = "Value")
+
 ## Plot
-hallmark_unique <- unique(df_long_tcga$Variable)
-for (hallmark in hallmark_unique){
-  df_subset <- subset(df_long_tcga, Variable == hallmark)
+hallmark_unique <- unique(df_final_hallmark_long$Variable)
+# for (hallmark in hallmark_unique){
+#   df_subset <- subset(df_final_hallmark_long, Variable == hallmark)
+#   p <- ggplot(df_subset,
+#               aes(x = factor(Class, levels = c("IRE", "TCGA")),
+#                   y = Value, fill = factor(Signature, levels = c("Low", "High")))) +
+#     geom_boxplot(outlier.size = 1, outlier.colour = "black",
+#                  width = 0.7, colour = "black", alpha = 0.8) +
+#     labs(title = paste0(hallmark),
+#          x = "Signature",
+#          y = "Value Expression",
+#          fill = "") +
+#     scale_fill_manual(values = c("Low" = "#1f77b4", "High" = "#d62728")) +
+#     theme_minimal(base_size = 14) +
+#     theme(legend.position = "",
+#           axis.text.x = element_text(angle = 45, hjust = 1, size = 12, face = "plain"),
+#           axis.text.y = element_text(size = 12, face = "plain"),
+#           plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
+#           axis.title = element_text(size = 14, face = "bold"),
+#           panel.grid.major = element_line(size = 0.2, color = "gray90"),
+#           panel.grid.minor = element_blank(),
+#           plot.background = element_rect(fill = "white", color = "white")) +
+#     stat_compare_means(method = "kruskal.test", label = "p.signif", label.x = 1.5, size = 5)
+#   ggsave(paste0("C:/Users/david/Documents/IFO/Final_Pipeline_Code/Output_Images/Hallmark/",
+#                 hallmark, "_boxplot.png"), plot = p, width = 8, height = 6)}
+
+
+## ***************************************
+## PROGENY
+## ***************************************
+net <- decoupleR::get_progeny(organism = "human", 
+                              top = 500)
+## MLM Model
+sample_acts <- decoupleR::run_mlm(mat = df_tpm_own_filtered_tumor, 
+                                  net = net, 
+                                  .source = "source", 
+                                  .target = "target",
+                                  .mor = "weight", 
+                                  minsize = 5)
+sample_acts_tcga <- decoupleR::run_mlm(mat = df_tpm_tcga_cleaned_tumor, 
+                                       net = net, 
+                                       .source = "source", 
+                                       .target = "target",
+                                       .mor = "weight", 
+                                       minsize = 5)
+## Transform to wide matrix
+sample_acts_mat <- sample_acts %>%
+  tidyr::pivot_wider(id_cols = "condition", 
+                     names_from = "source",
+                     values_from = "score") %>%
+  tibble::column_to_rownames("condition") %>%
+  as.matrix()
+sample_acts_mat_tcga <- sample_acts_tcga %>%
+  tidyr::pivot_wider(id_cols = "condition", 
+                     names_from = "source",
+                     values_from = "score") %>%
+  tibble::column_to_rownames("condition") %>%
+  as.matrix()
+
+## Scale per feature
+sample_acts_mat <- scale(sample_acts_mat)
+sample_acts_mat <- data.frame(sample_acts_mat)
+sample_acts_mat_tcga <- scale(sample_acts_mat_tcga)
+sample_acts_mat_tcga <- data.frame(sample_acts_mat_tcga)
+
+## Attach Signature
+sample_acts_mat$Signature      <- df_signature_own$Signature
+sample_acts_mat$Class          <- rep("IRE", nrow(sample_acts_mat))
+sample_acts_mat_tcga$Signature <- df_signature_tcga$Signature
+sample_acts_mat_tcga$Class     <- rep("TCGA", nrow(sample_acts_mat_tcga))
+## Merge
+sample_acts_final <- rbind(sample_acts_mat, sample_acts_mat_tcga)
+
+## Reshape
+sample_acts_final_long <- tidyr::pivot_longer(sample_acts_final, cols = colnames(sample_acts_final)[1:14], 
+                                              names_to = "Variable", values_to = "Value")
+
+## Plot
+pro_unique <- unique(sample_acts_final_long$Variable)
+for (pro in pro_unique){
+  df_subset <- subset(sample_acts_final_long, Variable == pro)
   p <- ggplot(df_subset,
-              aes(x = factor(Signature, levels = c("Low", "High")),
-                  y = Value, fill = Signature)) +
+              aes(x = factor(Class, levels = c("IRE", "TCGA")),
+                  y = Value, fill = factor(Signature, levels = c("Low", "High")))) +
     geom_boxplot(outlier.size = 1, outlier.colour = "black",
                  width = 0.7, colour = "black", alpha = 0.8) +
-    labs(title = paste0(hallmark, "- TCGA"),
+    labs(title = paste0(pro),
          x = "Signature",
          y = "Value Expression",
          fill = "") +
@@ -227,60 +247,8 @@ for (hallmark in hallmark_unique){
           panel.grid.minor = element_blank(),
           plot.background = element_rect(fill = "white", color = "white")) +
     stat_compare_means(method = "kruskal.test", label = "p.signif", label.x = 1.5, size = 5)
-  ggsave(paste0("C:/Users/david/Documents/IFO/Final_Pipeline_Code/Output_Images/Hallmark/TCGA/",
-                hallmark, "_boxplot.png"), plot = p, width = 8, height = 6)}
-
-
-## Heatmap
-## OWN
-ssGSEA_scores_scale_final_signature <- ssGSEA_scores_scale_final$Signature
-ssGSEA_scores_scale_final$Signature <- NULL
-## Annotation
-annotation_col <- data.frame(Group = ssGSEA_scores_scale_final_signature)
-rownames(annotation_col) <- colnames(t(ssGSEA_scores_scale_final))
-annotation_colors <- list(Group = c("Low" = "#1f77b4", "High" = "#d62728"))
-## Plot
-pheatmap(t(ssGSEA_scores_scale_final),
-         annotation_col = annotation_col,  
-         annotation_colors = annotation_colors,
-         scale = "row",  
-         fontsize = 12,  
-         main = "Hallmark - IRE",
-         cluster_rows = TRUE,
-         cluster_cols = FALSE,
-         show_rownames = TRUE,
-         show_colnames = FALSE)
-
-## TCGA
-ssGSEA_scores_scale_final_tcga_signature <- ssGSEA_scores_scale_final_tcga$Signature
-ssGSEA_scores_scale_final_tcga$Signature <- NULL
-## Annotation
-annotation_col <- data.frame(Group = ssGSEA_scores_scale_final_tcga_signature)
-rownames(annotation_col) <- colnames(t(ssGSEA_scores_scale_final_tcga))
-annotation_colors <- list(Group = c("Low" = "#1f77b4", "High" = "#d62728"))
-## Plot
-pheatmap(t(ssGSEA_scores_scale_final_tcga),
-         annotation_col = annotation_col,  
-         annotation_colors = annotation_colors,
-         scale = "row",  
-         fontsize = 12,  
-         main = "Hallmark - TCGA",
-         cluster_rows = TRUE,
-         cluster_cols = FALSE,
-         show_rownames = TRUE,
-         show_colnames = FALSE)
-
-## ***************************************
-## PROGENY
-## ***************************************
-net <- decoupleR::get_progeny(organism = "human", 
-                              top = 500)
-
-
-
-
-
-
+  ggsave(paste0("C:/Users/david/Documents/IFO/Final_Pipeline_Code/Output_Images/Progeny/",
+                pro, "_boxplot.png"), plot = p, width = 8, height = 6)}
 
 
 
